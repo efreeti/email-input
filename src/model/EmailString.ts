328810a02bb7cbec3721ac6e51d62099b9dd18cd
Bohdan Tsymbala <bohdan.tsymbala@ing.nl>
2020-05-12 11:27:29 +0200

    Added utility to parse emails from string.

--- src/model/EmailString.ts
+++ src/model/EmailString.ts
@@ -1,6 +1,14 @@
 const EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export class EmailString {
+	static parseEmailStringsFromString(string: string) {
+		return string
+			.replace(/^\s+|\s+$/g, '')
+			.split(/\s*,\s*/g)
+			.filter(string => string.length > 0)
+			.map(string => new EmailString(string));
+	}
+
 	constructor(public value: string) {
 	}
 
--- test/model/EmailString.spec.ts
+++ test/model/EmailString.spec.ts
@@ -1,6 +1,42 @@
 import {EmailString} from "../../src/model/EmailString";
 
 describe('EmailString', () => {
+	describe('EmailString::parseEmailStringsFromString', () => {
+		it('should return empty list when blank string', () => {
+			expect(EmailString.parseEmailStringsFromString(' \n\t    ')).toEqual([]);
+		});
+
+		it('should return correct list when single email string', () => {
+			expect(EmailString.parseEmailStringsFromString('bohdan@tsymbala.com')).toEqual([
+				new EmailString('bohdan@tsymbala.com')
+			]);
+		});
+
+		it('should return correct list when multiple emails string', () => {
+			expect(EmailString.parseEmailStringsFromString('bohdan@tsymbala.com,john@doe.com,jane@doe.com')).toEqual([
+				new EmailString('bohdan@tsymbala.com'),
+				new EmailString('john@doe.com'),
+				new EmailString('jane@doe.com')
+			]);
+		});
+
+		it('should return correct list when multiple emails string with trailing coma', () => {
+			expect(EmailString.parseEmailStringsFromString('bohdan@tsymbala.com,john@doe.com,jane@doe.com,')).toEqual([
+				new EmailString('bohdan@tsymbala.com'),
+				new EmailString('john@doe.com'),
+				new EmailString('jane@doe.com')
+			]);
+		});
+
+		it('should handle trailing spaces and spaces around coma correctly', () => {
+			expect(EmailString.parseEmailStringsFromString('\t\n bohdan@tsymbala.com ,john@doe.com , \njane@doe.com\t, \t\n')).toEqual([
+				new EmailString('bohdan@tsymbala.com'),
+				new EmailString('john@doe.com'),
+				new EmailString('jane@doe.com')
+			]);
+		});
+	});
+
 	describe('EmailString::isValid', () => {
 		it('should return true when valid', () => {
 			expect(new EmailString('bohdan@tsymbala.com').isValid()).toBeTruthy();
